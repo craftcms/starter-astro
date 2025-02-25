fc740f602146b28dc8be3843e54d4e164a37f55e
Travis Gertz <trav@louderthanten.com>
2025-02-25 22:44:49 +0700

    Use env variable in config

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,6 +3,12 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import vue from '@astrojs/vue';
 import tailwindcss from '@tailwindcss/vite';
+import { loadEnv } from 'vite';
+
+const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
+const site = env.BASE_URL;
+const siteUrl = new URL(site);
+
 export default defineConfig({
   output: 'server',
   adapter: node({
@@ -11,6 +17,7 @@ export default defineConfig({
   integrations: [
     vue(),
   ],
+  site: site,
   server: {
     host: '0.0.0.0',
     port: 4321,
@@ -20,7 +27,7 @@ export default defineConfig({
     server: {
       hmr: {
         protocol: 'wss',
-        host: 'starter-astro.ddev.site',
+        host: siteUrl.hostname,
         clientPort: 443,
       },
       host: '0.0.0.0'
