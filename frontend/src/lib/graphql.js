9a5ced0da209cf28fbd7299aea0799091f2ccc06
Travis Gertz <trav@louderthanten.com>
2025-05-11 10:08:38 +0800

    Simplify graphql libs

--- frontend/src/lib/graphql.js
+++ frontend/src/lib/graphql.js
@@ -70,4 +70,12 @@ export class GraphQLClient {
       throw err;
     }
   }
-} 
+}
+
+const gqlHost = import.meta.env.PUBLIC_GQL_HOST;
+if (!gqlHost) {
+  throw new Error('PUBLIC_GQL_HOST environment variable is not set');
+}
+const gqlUrl = new URL(gqlHost).toString();
+console.log('Initializing GraphQL client with:', { gqlUrl });
+export const craftClient = new GraphQLClient(gqlUrl); 
